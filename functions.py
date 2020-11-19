882eaafef6b66e611fc968ace72df7363446f661
SolarCoder <74742542+solar-coder@users.noreply.github.com>
2020-11-19 17:26:28 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,18 @@
+# Author: SolarCoder
+# Instagram | @solarcoder
+
+# defining the function
+# empty parentheses because hello() takes no parameters
+def hello():
+    # function body
+
+    # this is the code that is executed
+    # when the function is called
+    
+    print("Hello SolarCoders!")
+
+# calling the function
+# (executing the code inside hello())
+hello()
+
+# Output: Hello SolarCoders!
